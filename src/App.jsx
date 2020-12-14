84df486ecb07739583afd36e060fecb7797d71e7
ashablyka <an.shablyka@gmail.com>
2020-12-14 08:46:16 +0300

    feat: add component Table to App with new styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Date from './components/Date/Date';
 import Switcher from './components/Switcher/Switcher';
 import switchers from './components/switchers';
 import Map from './components/Map/Map';
+import Table from './components/Table/Table';
 import s from './App.module.css';
 
 export default function App() {
@@ -21,7 +22,15 @@ export default function App() {
           <Map />
         </div>
       </div>
-      <div className={s['statistic-container']} />
+      <div className={s['statistic-container']}>
+        <div className={s['statistic-container__switchers']}>
+          <Switcher switchData={switchers.casesSwitcher} />
+          <Switcher switchData={switchers.timeSwitcher} />
+        </div>
+        <div className={s['statistic-container__table']}>
+          <Table />
+        </div>
+      </div>
       <div className={s['chart-container']} />
       <div className={s['footer-container']} />
     </div>
