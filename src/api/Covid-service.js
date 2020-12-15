cc79ea2db3e404884cc7ec855130f9055f1484e3
ashablyka <an.shablyka@gmail.com>
2020-12-15 14:37:27 +0300

    feat: add covid api service

---
+++ src/api/Covid-service.js
@@ -0,0 +1,18 @@
+export default class CovidService {
+  covid19apiUrl = 'https://api.covid19api.com';
+
+  static async getResource(url) {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Network response from ${url} was not ok. Response status is ${response.status}`);
+    }
+
+    return response.json();
+  }
+
+  async getSummary() {
+    const summary = await CovidService.getResource(`${this.covid19apiUrl}/summary`);
+    return summary;
+  }
+}
