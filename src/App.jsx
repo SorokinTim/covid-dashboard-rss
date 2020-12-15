b6101dc7e8dceb9b0a0bc5955f3f9cbeb0b59277
ashablyka <an.shablyka@gmail.com>
2020-12-15 14:40:34 +0300

    feat: output table figures from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CovidService from './api/Covid-service';
 import Date from './components/Date/Date';
 import Switcher from './components/Switcher/Switcher';
 import switchers from './components/switchers';
@@ -8,44 +9,77 @@ import Map from './components/Map/Map';
 import Table from './components/Table/Table';
 import s from './App.module.css';
 
-export default function App() {
-  return (
-    <div className={s.container}>
-      <div className={s['date-container']}>
-        <Date />
+export default class App extends Component {
+  covidService = new CovidService();
+
+  constructor() {
+    super();
+    this.state = {
+      summary: null,
+    };
+  }
+
+  componentDidMount() {
+    this.getSummary();
+  }
+
+  getSummary() {
+    this.covidService
+      .getSummary()
+      .then((summary) => {
+        this.setState({
+          summary,
+        });
+      });
+  }
+
+  render() {
+    const { summary } = this.state;
+    const global = summary ? summary.Global : null;
+    const tableData = global ? {
+      confirmed: global.TotalConfirmed,
+      deaths: global.TotalDeaths,
+      recovered: global.TotalRecovered,
+    } : null;
+
+    return (
+      <div className={s.container}>
+        <div className={s['date-container']}>
+          <Date />
+        </div>
+        <div className={s['details-container']}>
+          <div className={s['details-container__switchers']}>
+            <Switcher switchData={switchers.casesSwitcher} />
+            <Switcher switchData={switchers.timeSwitcher} />
+          </div>
+          <div className={s['details-container__search']}>
+            <Search />
+          </div>
+          <div className={s['details-container__countries-list']}>
+            <List />
+          </div>
+        </div>
+        <div className={s['map-container']}>
+          <div className={s['map-container__switchers']}>
+            <Switcher switchData={switchers.casesSwitcher} />
+            <Switcher switchData={switchers.timeSwitcher} />
+          </div>
+          <div className={s['map-container__map']}>
+            <Map />
+          </div>
+        </div>
+        <div className={s['statistic-container']}>
+          <div className={s['statistic-container__switchers']}>
+            <Switcher switchData={switchers.casesSwitcher} />
+            <Switcher switchData={switchers.timeSwitcher} />
+          </div>
+          <div className={s['statistic-container__table']}>
+            <Table tableData={tableData} />
+          </div>
+        </div>
+        <div className={s['chart-container']} />
+        <div className={s['footer-container']} />
       </div>
-      <div className={s['details-container']}>
-        <div className={s['details-container__switchers']}>
-          <Switcher switchData={switchers.casesSwitcher} />
-          <Switcher switchData={switchers.timeSwitcher} />
-        </div>
-        <div className={s['details-container__search']}>
-          <Search />
-        </div>
-        <div className={s['details-container__countries-list']}>
-          <List />
-        </div>
-      </div>
-      <div className={s['map-container']}>
-        <div className={s['map-container__switchers']}>
-          <Switcher switchData={switchers.casesSwitcher} />
-          <Switcher switchData={switchers.timeSwitcher} />
-        </div>
-        <div className={s['map-container__map']}>
-          <Map />
-        </div>
-      </div>
-      <div className={s['statistic-container']}>
-        <div className={s['statistic-container__switchers']}>
-          <Switcher switchData={switchers.casesSwitcher} />
-          <Switcher switchData={switchers.timeSwitcher} />
-        </div>
-        <div className={s['statistic-container__table']}>
-          <Table />
-        </div>
-      </div>
-      <div className={s['chart-container']} />
-      <div className={s['footer-container']} />
-    </div>
-  );
+    );
+  }
 }
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,23 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import s from './Table.module.css';
 
-export default function Table() {
-  return (
-    <table className={s.table}>
-      <thead className={s.table__head}>
-        <tr className={s.table__row}>
-          <th className={s['table__head-cell']}>Confirmed</th>
-          <th className={s['table__head-cell']}>Deaths</th>
-          <th className={s['table__head-cell']}>Recovered</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className={s.table__row}>
-          <td className={s.table__cell}>69,546,321</td>
-          <td className={s.table__cell}>44,546,321</td>
-          <td className={s.table__cell}>1,546,321</td>
-        </tr>
-      </tbody>
-    </table>
-  );
+export default class Table extends Component {
+  constructor() {
+    super();
+    this.state = {
+      confirmed: null,
+      deaths: null,
+      recovered: null,
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tableData } = this.props;
+
+    if (tableData.confirmed !== prevState.confirmed) {
+      this.onComponentUpdate();
+    }
+  }
+
+  onComponentUpdate() {
+    const { tableData } = this.props;
+
+    this.setState({
+      confirmed: tableData.confirmed,
+      deaths: tableData.deaths,
+      recovered: tableData.recovered,
+    });
+  }
+
+  render() {
+    const { confirmed, deaths, recovered } = this.state;
+
+    return (
+      <table className={s.table}>
+        <thead className={s.table__head}>
+          <tr className={s.table__row}>
+            <th className={s['table__head-cell']}>Confirmed</th>
+            <th className={s['table__head-cell']}>Deaths</th>
+            <th className={s['table__head-cell']}>Recovered</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className={s.table__row}>
+            <td className={s.table__cell}>{confirmed}</td>
+            <td className={s.table__cell}>{deaths}</td>
+            <td className={s.table__cell}>{recovered}</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
 }
+
+Table.defaultProps = {
+  tableData: null,
+};
+
+Table.propTypes = {
+  tableData: PropTypes.shape({
+    confirmed: PropTypes.number,
+    deaths: PropTypes.number,
+    recovered: PropTypes.number,
+  }),
+};
