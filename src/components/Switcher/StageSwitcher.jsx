234ac35772e3b02dc701c24c8fba4b1802b26ac6
Tim Sorokin <timsorokin352@gmail.com>
2020-12-22 03:30:36 +0300

    feat: change Switcher style, remove mapbox label

--- src/components/Switcher/StageSwitcher.jsx
+++ src/components/Switcher/StageSwitcher.jsx
@@ -21,7 +21,7 @@ export default function StageSwitcher({ groupName, switchersState, onSwitcherCha
         className={style['choice-1']}
         onChange={(e) => onSwitcherChange(e.target.value)}
       />
-      <label htmlFor={CONFIRMED} className={style.switcher__item} to="1">Confirmed</label>
+      <label htmlFor={CONFIRMED} className={`${style.switcher__item} ${style.switcher__item_big}`} to="1">Confirmed</label>
 
       <input
         type="radio"
@@ -32,7 +32,7 @@ export default function StageSwitcher({ groupName, switchersState, onSwitcherCha
         className={style['choice-2']}
         onChange={(e) => onSwitcherChange(e.target.value)}
       />
-      <label htmlFor={DEATHS} className={style.switcher__item} to="2">Deaths</label>
+      <label htmlFor={DEATHS} className={`${style.switcher__item} ${style.switcher__item_big}`} to="2">Deaths</label>
 
       <input
         type="radio"
@@ -43,7 +43,7 @@ export default function StageSwitcher({ groupName, switchersState, onSwitcherCha
         className={style['choice-3']}
         onChange={(e) => onSwitcherChange(e.target.value)}
       />
-      <label htmlFor={RECOVERED} className={style.switcher__item} to="3">Recovered</label>
+      <label htmlFor={RECOVERED} className={`${style.switcher__item} ${style.switcher__item_big}`} to="3">Recovered</label>
     </div>
   );
 }
