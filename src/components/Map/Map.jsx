5af0cc15717dc7c903f940dd64f909eaeac04d16
ashablyka <an.shablyka@gmail.com>
2020-12-24 22:19:12 +0300

    refactor: add props checking

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import mapboxgl from '../../api/api-mapbox';
 import style from './Map.module.css';
 import getRequiredParam from '../../utils/getRequiredParam';
@@ -11,67 +12,70 @@ function findCountryCoords(startData, country) {
   return [long, lat];
 }
 
+function getMarkerLabelHTML(props) {
+  return (`
+    <h3>${props.title}</h3>
+    <br>
+    <p>Cases: ${props.cases}</p>
+  `);
+}
+
+function getMarkerLabel(props) {
+  const popup = new mapboxgl.Popup({ offset: 25, className: style.map__popup })
+    .setHTML(getMarkerLabelHTML(props));
+
+  return popup;
+}
+
+function getMapMarker(props) {
+  const el = document.createElement('div');
+  el.className = style.map__marker;
+
+  // TODO: change size
+  // const size = TODO: your variable value;
+  // el.style.width = `${size}px`;
+  // el.style.height = `${size}px`;
+
+  new mapboxgl.Marker(el).setLngLat(props.coords)
+    .setPopup(getMarkerLabel(props)).addTo(props.map);
+}
 export default class Map extends React.Component {
   constructor() {
     super();
     this.mapRef = React.createRef();
   }
 
-  componentDidUpdate() {
-    const coord = findCountryCoords(this.props.startData, this.props.country);
-    this.initializeMap(coord);
-  }
-
   componentDidMount() {
     this.initializeMap();
   }
 
-  initializeMap(coord = undefined) {
+  componentDidUpdate() {
+    const { startData, country } = this.props;
+    const coords = findCountryCoords(startData, country);
+    this.initializeMap(coords);
+  }
+
+  initializeMap(coords = undefined) {
     this.map = new mapboxgl.Map({
       container: this.mapRef,
       style: 'mapbox://styles/mapbox/dark-v9',
-      center: coord !== undefined ? coord : [29.632, 54.257],
+      center: coords !== undefined ? coords : [29.632, 54.257],
       zoom: 3.5,
     });
     // const globalCases = fn();
-    this.props.startData.forEach((el) => {
-      this.getMapMarker({
+    const { startData, switchersState } = this.props;
+
+    startData.forEach((el) => {
+      getMapMarker({
         map: this.map,
-        coord: [el.countryInfo.long, el.countryInfo.lat],
+        coords: [el.countryInfo.long, el.countryInfo.lat],
         title: el.country,
-        cases: getRequiredParam(el, this.props.switchersState),
-      //  size: country.cases / globalCases * 100
+        cases: getRequiredParam(el, switchersState),
+        //  size: country.cases / globalCases * 100
       });
     });
 
     return this.map;
-  }
-
-  getMapMarker(props) {
-    const el = document.createElement('div');
-    el.className = style.map__marker;
-
-    // TODO: change size
-    // const size = TODO: your variable value;
-    // el.style.width = `${size}px`;
-    // el.style.height = `${size}px`;
-
-    new mapboxgl.Marker(el).setLngLat(props.coord).setPopup(this.getMarkerLabel(props)).addTo(props.map);
-  }
-
-  getMarkerLabel(props) {
-    const popup = new mapboxgl.Popup({ offset: 25, className: style.map__popup })
-      .setHTML(this.getMarkerLabelHTML(props));
-
-    return popup;
-  }
-
-  getMarkerLabelHTML(props) {
-    return (`
-      <h3>${props.title}</h3>
-      <br>
-      <p>Cases: ${props.cases}</p>
-    `);
   }
 
   render() {
@@ -82,3 +86,28 @@ export default class Map extends React.Component {
     );
   }
 }
+
+Map.defaultProps = {
+  country: null,
+};
+
+Map.propTypes = {
+  startData: PropTypes.arrayOf(
+    PropTypes.shape({
+      updated: PropTypes.number,
+      country: PropTypes.string,
+      cases: PropTypes.number,
+      deaths: PropTypes.number,
+      recovered: PropTypes.number,
+      countryInfo: PropTypes.shape({
+        flag: PropTypes.string,
+      }),
+    }),
+  ).isRequired,
+  country: PropTypes.string,
+  switchersState: PropTypes.shape({
+    partOfPopulation: PropTypes.string,
+    typeOfTime: PropTypes.string,
+    stageOfDisease: PropTypes.string,
+  }).isRequired,
+};
