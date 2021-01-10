fc491764e8e952b1a98825af6700d6dae465673a
ashablyka <an.shablyka@gmail.com>
2021-01-10 11:46:13 +0300

    refactor: correct descriptions

--- src/utils/roundFigure.test.js
+++ src/utils/roundFigure.test.js
@@ -1,9 +1,9 @@
 import roundFigure from './roundFigure';
 
-test('round 1.21 to to first decimal place is equal 1.2', () => {
+test('round 1.21 to first decimal place is equal 1.2', () => {
   expect(roundFigure(1.21, 1)).toBe(1.2);
 });
 
-test('round 1.21 to to second decimal place is equal 1.2', () => {
+test('round 1.256 to second decimal place is equal 1.26', () => {
   expect(roundFigure(1.256, 2)).toBe(1.26);
 });
