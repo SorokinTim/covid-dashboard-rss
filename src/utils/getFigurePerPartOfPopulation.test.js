d5af184882aad0bcfda1276d0f66d75541ed2a81
ashablyka <an.shablyka@gmail.com>
2021-01-10 11:17:19 +0300

    refactor: describe numbers

--- src/utils/getFigurePerPartOfPopulation.test.js
+++ src/utils/getFigurePerPartOfPopulation.test.js
@@ -1,5 +1,5 @@
 import getFigurePerPartOfPopulation from './getFigurePerPartOfPopulation';
 
-test('15 per 1 million is 1.5 per 100k', () => {
+test('15 people per 1 million population is 1.5 people per 100k population', () => {
   expect(getFigurePerPartOfPopulation(15, 1000000)).toBe(1.5);
 });
